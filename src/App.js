97f49d38adf6948502c866e5635019a053904dca
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2021-05-28 15:45:35 +0530

    Updated UI, Arranged new dynamic animated rows

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
 import RowPost from './Components/RowPost/RowPost';
+import {action, orginals, comedy, romance} from './url'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost url={orginals} title={"Netflix Orginals"} />
+      <RowPost url={action} title={"Action"} isSmall />
+      <RowPost url={comedy} title={"Comedy"} isSmall />
+      <RowPost url={romance} title={"Romance"} isSmall />
     </div>
   );
 }
